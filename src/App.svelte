b077458d1f3d6e16d1f966afb5caa861dfce0517
Thiago Hersan <thiago.hersan+github@gmail.com>
2022-01-27 22:35:20 -0500

    adds filters to list

--- src/App.svelte
+++ src/App.svelte
@@ -3,43 +3,60 @@
 	import Cordial from './Cordial.svelte';
 
 	let allObras: Array<CordialType>;
+	let filteredObras: Array<CordialType>;
 	let selectedObra: CordialType;
 	let obrasReady = false;
+	let filterNudes = false;
+	let filterMarcantonio = false;
+	let filterNeutral = false;
+	let filterCut = 50;
 
 	async function getObras() {
 		let response = await fetch(`${window.location.href}data/obras.json`);
 		let obras = await response.json();
-		return Object.keys(obras).map(o => obras[o]);
+		const obrasList = Object.keys(obras).map(o => obras[o]);
+		return obrasList.sort((a, b) => a.artist.localeCompare(b.artist));
 	}
 
 	getObras().then((obras) => {
 		allObras = obras.filter(obra => 'emotions' in obra);
-		// allObras = allObras.filter(obra => obra.emotions.neutral < 50);
-		selectedObra = allObras[0];
+		filteredObras = [...allObras];
+		selectedObra = filteredObras[0];
 		obrasReady = true;
 	});
+
+	let updateFiltered = () => {
+		filteredObras = [...allObras];
+		if (filterNudes)
+			filteredObras = filteredObras.filter(obra => obra.nudes);
+		if (filterMarcantonio)
+			filteredObras = filteredObras.filter(obra => obra.marcantonio);
+		if (filterNeutral)
+			filteredObras = filteredObras.filter(obra => obra.emotions.neutral <= filterCut);
+
+		filteredObras = filteredObras;
+		selectedObra = filteredObras[0];
+	}
 </script>
 
-<main>
-	{#if obrasReady}
-		<select bind:value={selectedObra} >
-			{#each allObras as obra}
-				<option value={obra}>{obra.title}</option>
-			{/each}
-		</select>
-		<Cordial obra={selectedObra} />
-	{/if}
-</main>
+<label>
+	<input type=checkbox bind:checked={filterNudes} on:change={updateFiltered}>
+	Nudes
+</label>
+<label>
+	<input type=checkbox bind:checked={filterMarcantonio} on:change={updateFiltered}>
+	Marcantonio
+</label>
+<label>
+	<input type=checkbox bind:checked={filterNeutral} on:change={updateFiltered}>
+	Neutro &lt; <input bind:value={filterCut}>%
+</label>
 
-<style>
-	main {
-		max-width: 600px;
-		margin: 0 auto;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+{#if obrasReady}
+	<select bind:value={selectedObra} >
+		{#each filteredObras as obra}
+			<option value={obra}>{obra.artist} - {obra.title}</option>
+		{/each}
+	</select>{filteredObras.length}
+	<Cordial obra={selectedObra} />
+{/if}
