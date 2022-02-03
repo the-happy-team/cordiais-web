e162e71a066cd0efb05d78879807c99a0b3286b5
Thiago Hersan <thiago.hersan+github@gmail.com>
2022-02-02 21:53:52 -0500

    formatting svelte files

--- src/App.svelte
+++ src/App.svelte
@@ -1,84 +1,97 @@
 <script lang="ts">
-	import type { CordialType } from './types/cordiais.types';
-	import Cordial from './Cordial.svelte';
+  import type { CordialType } from "./types/cordiais.types";
+  import Cordial from "./Cordial.svelte";
 
-	let allObras: Array<CordialType>;
-	let filteredObras: Array<CordialType>;
-	let selectedObra: CordialType;
-	let obrasReady = false;
-	let filterNudes = false;
-	let filterMarcantonio = false;
-	let filterNeutral = false;
-	let filterCut = 50;
+  let allObras: Array<CordialType>;
+  let filteredObras: Array<CordialType>;
+  let selectedObra: CordialType;
+  let obrasReady = false;
+  let filterNudes = false;
+  let filterMarcantonio = false;
+  let filterNeutral = false;
+  let filterCut = 50;
 
-	async function getObras() {
-		let response = await fetch(`${window.location.href}data/obras.json`);
-		let obras = await response.json();
-		const obrasList = Object.keys(obras).map(o => obras[o]);
-		return obrasList.sort((a, b) => a.artist.localeCompare(b.artist));
-	}
+  async function getObras() {
+    let response = await fetch(`${window.location.href}data/obras.json`);
+    let obras = await response.json();
+    const obrasList = Object.keys(obras).map((o) => obras[o]);
+    return obrasList.sort((a, b) => a.artist.localeCompare(b.artist));
+  }
 
-	getObras().then((obras) => {
-		allObras = obras.filter(obra => 'emotions' in obra);
-		filteredObras = [...allObras];
-		selectedObra = filteredObras[0];
-		obrasReady = true;
-	});
+  getObras().then((obras) => {
+    allObras = obras.filter((obra) => "emotions" in obra);
+    filteredObras = [...allObras];
+    selectedObra = filteredObras[0];
+    obrasReady = true;
+  });
 
-	let updateFiltered = () => {
-		filteredObras = [...allObras];
-		if (filterNudes)
-			filteredObras = filteredObras.filter(obra => obra.nudes);
-		if (filterMarcantonio)
-			filteredObras = filteredObras.filter(obra => obra.marcantonio);
-		if (filterNeutral)
-			filteredObras = filteredObras.filter(obra => obra.emotions.neutral <= filterCut);
+  let updateFiltered = () => {
+    filteredObras = [...allObras];
+    if (filterNudes) filteredObras = filteredObras.filter((obra) => obra.nudes);
+    if (filterMarcantonio)
+      filteredObras = filteredObras.filter((obra) => obra.marcantonio);
+    if (filterNeutral)
+      filteredObras = filteredObras.filter(
+        (obra) => obra.emotions.neutral <= filterCut
+      );
 
-		selectedObra = filteredObras[0];
-	}
+    selectedObra = filteredObras[0];
+  };
 </script>
 
 <div class="filters">
-	<div class="filter-group">
-		<input type=checkbox bind:checked={filterNudes} on:change={updateFiltered}>
-		Nudes
-	</div>
-	<div class="filter-group">
-		<input type=checkbox bind:checked={filterMarcantonio} on:change={updateFiltered}>
-		Marcantonio
-	</div>
-	<div class="filter-group">
-		<input type=checkbox bind:checked={filterNeutral} on:change={updateFiltered}>
-		Neutro &lt; <input bind:value={filterCut} on:change={updateFiltered}>%
-	</div>
+  <div class="filter-group">
+    <input
+      type="checkbox"
+      bind:checked={filterNudes}
+      on:change={updateFiltered}
+    />
+    Nudes
+  </div>
+  <div class="filter-group">
+    <input
+      type="checkbox"
+      bind:checked={filterMarcantonio}
+      on:change={updateFiltered}
+    />
+    Marcantonio
+  </div>
+  <div class="filter-group">
+    <input
+      type="checkbox"
+      bind:checked={filterNeutral}
+      on:change={updateFiltered}
+    />
+    Neutro &lt; <input bind:value={filterCut} on:change={updateFiltered} />%
+  </div>
 </div>
 
 {#if obrasReady}
-	<select bind:value={selectedObra} >
-		{#each filteredObras as obra}
-			<option value={obra}>{obra.artist} - {obra.title}</option>
-		{/each}
-	</select>{filteredObras.length}
-	{#if filteredObras.length > 0}
-		<Cordial obra={selectedObra} />
-	{/if}
+  <select bind:value={selectedObra}>
+    {#each filteredObras as obra}
+      <option value={obra}>{obra.artist} - {obra.title}</option>
+    {/each}
+  </select>{filteredObras.length}
+  {#if filteredObras.length > 0}
+    <Cordial obra={selectedObra} />
+  {/if}
 {/if}
 
 <style lang="scss">
-	.filters {
-		display: flex;
-		flex-direction: column;
+  .filters {
+    display: flex;
+    flex-direction: column;
 
-		.filter-group {
-			display: flex;
-			flex-direction: row;
-			align-content: center;
-			align-items: baseline;
-			margin: 4px 0;
+    .filter-group {
+      display: flex;
+      flex-direction: row;
+      align-content: center;
+      align-items: baseline;
+      margin: 4px 0;
 
-			input {
-				margin-right: 8px;
-			}
-		}
-	}
+      input {
+        margin-right: 8px;
+      }
+    }
+  }
 </style>
--- src/Cordial.svelte
+++ src/Cordial.svelte
@@ -1,84 +1,94 @@
 <script lang="ts">
-    import type { CordialType } from './types/cordiais.types';
-    import { EmotionOrder } from './types/cordiais.types';
+  import type { CordialType } from "./types/cordiais.types";
+  import { EmotionOrder } from "./types/cordiais.types";
 
-    export let obra: CordialType;
+  export let obra: CordialType;
 
-    let selectedEmotion: string;
+  let selectedEmotion: string;
 
-    let selectTopEmotion = (o: CordialType) => {
-        selectedEmotion = EmotionOrder.reduce((a, b) => o.emotions[a] > o.emotions[b] ? a : b);
-    };
+  let selectTopEmotion = (o: CordialType) => {
+    selectedEmotion = EmotionOrder.reduce((a, b) =>
+      o.emotions[a] > o.emotions[b] ? a : b
+    );
+  };
 
-    $: selectTopEmotion(obra);
+  $: selectTopEmotion(obra);
 </script>
 
 <div class="cordial">
-    <div class="title">{obra.title} - {obra.year}</div>
-    <div class="artist">{obra.artist}</div>
-    <div class="medium">
-        {obra.medium} / {`${obra.dimension.width} x ${obra.dimension.height} ${obra.dimension.unit}`}
-    </div>
-    <div class="collection">{obra.collection}</div>
+  <div class="title">{obra.title} - {obra.year}</div>
+  <div class="artist">{obra.artist}</div>
+  <div class="medium">
+    {obra.medium} / {`${obra.dimension.width} x ${obra.dimension.height} ${obra.dimension.unit}`}
+  </div>
+  <div class="collection">{obra.collection}</div>
 
-    <div class="emotion-list">
-        {#each EmotionOrder as e}
-            <label class="emotion-option" class:selected={selectedEmotion == e}>
-                <input type=radio bind:group={selectedEmotion} name="emotions" value={e}>
-                {`${e}: ${obra.emotions[e]}`}
-            </label>
-        {/each}
-    </div>
+  <div class="emotion-list">
+    {#each EmotionOrder as e}
+      <label class="emotion-option" class:selected={selectedEmotion == e}>
+        <input
+          type="radio"
+          bind:group={selectedEmotion}
+          name="emotions"
+          value={e}
+        />
+        {`${e}: ${obra.emotions[e]}`}
+      </label>
+    {/each}
+  </div>
 
-    <div class="images-container">
-        <div class="image" style={`background-image: url('./imgs/obras/${obra.img}');`}></div>
-    </div>
+  <div class="images-container">
+    <div
+      class="image"
+      style={`background-image: url('./imgs/obras/${obra.img}');`}
+    />
+  </div>
 </div>
 
 <style lang="scss">
-    .cordial {
-        .title {
-            color: rgb(128, 0, 0);
-        }
-        .artist {
-            color: rgb(0, 128, 0);
-        }
-        .medium {
-            color: rgb(0, 0, 128);
-        }
-        .collection {
-            color: hotpink;
-        }
-
-        .emotion-list {
-            width: max-content;
-            margin: 10px 0;
-        }
-
-        .emotion-option {
-            cursor: pointer;
-            line-height: 1.4;
-
-            &.selected {
-                color: cadetblue;
-            }
-            input[type=radio] {
-                display: none;
-            }
-        }
-
-        .images-container {
-            width: 100%;
-            display: flex;
-            flex-direction: row;
-
-            .image {
-                width: 33%;
-                height: 300px;
-                background-position: top left;
-                background-repeat: no-repeat;
-                background-size: contain;
-            }
-        }
+  .cordial {
+    .title {
+      color: rgb(128, 0, 0);
     }
+    .artist {
+      color: rgb(0, 128, 0);
+    }
+    .medium {
+      color: rgb(0, 0, 128);
+    }
+    .collection {
+      color: hotpink;
+    }
+
+    .emotion-list {
+      width: max-content;
+      margin: 10px 0;
+    }
+
+    .emotion-option {
+      cursor: pointer;
+      line-height: 1.4;
+
+      &.selected {
+        color: cadetblue;
+      }
+      input[type="radio"] {
+        display: none;
+      }
+    }
+
+    .images-container {
+      width: 100%;
+      display: flex;
+      flex-direction: row;
+
+      .image {
+        width: 33%;
+        height: 300px;
+        background-position: top left;
+        background-repeat: no-repeat;
+        background-size: contain;
+      }
+    }
+  }
 </style>
