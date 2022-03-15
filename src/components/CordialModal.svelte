f5a5e7b15062d049ad20f39d4bb7a4b955c6983b
Thiago Hersan <thiago.hersan+github@gmail.com>
2022-03-14 23:14:18 -0400

    adds CordialModal component

---
+++ src/components/CordialModal.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import type { CordialType } from "../types/cordiais.types";
+  import { EmotionOrder } from "../types/cordiais.types";
+
+  export let obra: CordialType;
+</script>
+
+<div>
+  {obra.title} ({obra.year})
+  {obra.slug}
+</div>
