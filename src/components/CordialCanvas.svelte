45264abf5bf255ec15a2b71d45d8cdedc391fa45
Thiago Hersan <thiago.hersan+github@gmail.com>
2022-03-16 10:09:53 -0400

    refactors canvas into own component

---
+++ src/components/CordialCanvas.svelte
@@ -0,0 +1,88 @@
+<script lang="ts">
+  import { onMount } from "svelte/internal";
+  import type { CordialType } from "../types/cordiais.types";
+  import { EmotionOrder } from "../types/cordiais.types";
+
+  export let obra: CordialType;
+  export let selectedEmotion = EmotionOrder[0];
+
+  let obraCanvas: HTMLCanvasElement;
+
+  const drawCanvas = (selectedEmotionValue: number) => {
+    if (!obraCanvas) return;
+
+    const emo2color = (emo: string) => {
+      if (emo == "neutral") return "#7f7f7f";
+      else return "#000000";
+    };
+
+    // clear canvas
+    const ctx = obraCanvas.getContext("2d");
+    ctx.fillStyle = "#ffffff";
+    ctx.fillRect(0, 0, obraCanvas.width, obraCanvas.height);
+
+    if (!obra.dimension.width || !obra.dimension.height) return;
+
+    // calculate background w,h
+    const obraRatio = obra.dimension.width / obra.dimension.height;
+    const obraBackground = {
+      height: obraCanvas.height,
+      width: obraRatio * obraCanvas.height,
+      x: 0,
+      y: 0,
+    };
+
+    if (obraBackground.width > obraCanvas.width) {
+      obraBackground.width = obraCanvas.width;
+      obraBackground.height = obraBackground.width / obraRatio;
+    }
+
+    obraBackground.x = (obraCanvas.width - obraBackground.width) / 2;
+    obraBackground.y = 0;
+
+    ctx.fillStyle = emo2color("neutral");
+    ctx.fillRect(
+      obraBackground.x,
+      obraBackground.y,
+      obraBackground.width,
+      obraBackground.height
+    );
+
+    // calculate color rectangle
+    const obWpH = obraBackground.width + obraBackground.height;
+    const obWH = obraBackground.width * obraBackground.height;
+    const emoNorm = 1.0 - 0.01 * selectedEmotionValue;
+    const borderWidth =
+      0.25 * (obWpH - Math.sqrt(obWpH * obWpH - 4 * emoNorm * obWH));
+
+    const obraColor = {
+      height: obraBackground.height - 2 * borderWidth,
+      width: obraBackground.width - 2 * borderWidth,
+      x: obraBackground.x + borderWidth,
+      y: obraBackground.y + borderWidth,
+    };
+
+    ctx.fillStyle = emo2color(selectedEmotion);
+    ctx.fillRect(obraColor.x, obraColor.y, obraColor.width, obraColor.height);
+  };
+
+  onMount(() => {
+    obraCanvas.setAttribute("width", `${2 * obraCanvas.offsetWidth}`);
+    obraCanvas.setAttribute("height", `${2 * obraCanvas.offsetHeight}`);
+  });
+
+  $: if (selectedEmotion) {
+    console.log("emo changed");
+    drawCanvas(obra.emotions[selectedEmotion]);
+  }
+</script>
+
+<canvas class="obra-canvas" bind:this={obraCanvas} />
+
+<style lang="scss">
+  .obra-canvas {
+    width: 100%;
+    height: 100%;
+    box-sizing: border-box;
+  }
+</style>
--- src/components/CordialModal.svelte
+++ src/components/CordialModal.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-  import { onMount } from "svelte/internal";
   import type { CordialType } from "../types/cordiais.types";
   import { EmotionOrder } from "../types/cordiais.types";
+  import CordialCanvas from "./CordialCanvas.svelte";
   import EmotionSelection from "./EmotionSelection.svelte";
   import Modal from "./Modal.svelte";
 
@@ -9,85 +9,13 @@
 
   const baseurl = window.location.href.replace(window.location.hash, "");
 
-  let obraCanvas: HTMLCanvasElement;
-
   let selectedEmotion = EmotionOrder[0];
-  let selectedEmotionValue: number;
 
   const handleClick = (e) => {
     e.stopPropagation();
   };
 
-  const drawCanvas = () => {
-    if (!obraCanvas) return;
-
-    const emo2color = (emo: string) => {
-      if (emo == "neutral") return "#7f7f7f";
-      else return "#000000";
-    };
-
-    // clear canvas
-    const ctx = obraCanvas.getContext("2d");
-    ctx.fillStyle = "#ffffff";
-    ctx.fillRect(0, 0, obraCanvas.width, obraCanvas.height);
-
-    if (!obra.dimension.width || !obra.dimension.height) return;
-
-    // calculate background w,h
-    const obraRatio = obra.dimension.width / obra.dimension.height;
-    const obraBackground = {
-      height: obraCanvas.height,
-      width: obraRatio * obraCanvas.height,
-      x: 0,
-      y: 0,
-    };
-
-    if (obraBackground.width > obraCanvas.width) {
-      obraBackground.width = obraCanvas.width;
-      obraBackground.height = obraBackground.width / obraRatio;
-    }
-
-    obraBackground.x = (obraCanvas.width - obraBackground.width) / 2;
-    obraBackground.y = 0;
-
-    ctx.fillStyle = emo2color("neutral");
-    ctx.fillRect(
-      obraBackground.x,
-      obraBackground.y,
-      obraBackground.width,
-      obraBackground.height
-    );
-
-    // calculate color rectangle
-    const obWpH = obraBackground.width + obraBackground.height;
-    const obWH = obraBackground.width * obraBackground.height;
-    const emoNorm = 1.0 - 0.01 * selectedEmotionValue;
-    const borderWidth =
-      0.25 * (obWpH - Math.sqrt(obWpH * obWpH - 4 * emoNorm * obWH));
-
-    const obraColor = {
-      height: obraBackground.height - 2 * borderWidth,
-      width: obraBackground.width - 2 * borderWidth,
-      x: obraBackground.x + borderWidth,
-      y: obraBackground.y + borderWidth,
-    };
-
-    ctx.fillStyle = emo2color(selectedEmotion);
-    ctx.fillRect(obraColor.x, obraColor.y, obraColor.width, obraColor.height);
-  };
-
-  onMount(() => {
-    obraCanvas.setAttribute("width", `${2 * obraCanvas.offsetWidth}`);
-    obraCanvas.setAttribute("height", `${2 * obraCanvas.offsetHeight}`);
-  });
-
   $: obraImgCss = `--obraImgUrl: url('${baseurl}imgs/obras/${obra.img}');`;
-
-  $: if (selectedEmotion) {
-    console.log("emo changed");
-    selectedEmotionValue = obra.emotions[selectedEmotion];
-    drawCanvas();
-  }
 </script>
 
 <Modal visible={obra != null} on:close={() => (obra = null)}>
@@ -99,11 +27,11 @@
       </div>
 
       <div class="emotion-selection-container">
-        <EmotionSelection bind:selectedEmotion {obra} />
+        <EmotionSelection {obra} bind:selectedEmotion />
       </div>
 
       <div class="canvas-container">
-        <canvas class="obra-canvas" bind:this={obraCanvas} />
+        <CordialCanvas {obra} {selectedEmotion} />
       </div>
     </div>
   </div>
@@ -160,12 +88,6 @@
       flex-basis: 0;
       height: 100%;
       box-sizing: border-box;
-
-      .obra-canvas {
-        width: 100%;
-        height: 100%;
-        box-sizing: border-box;
-      }
     }
   }
 </style>
