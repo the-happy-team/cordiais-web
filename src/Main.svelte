78d5a32a88e79fb522274179025ee9568c51539c
Thiago Hersan <thiago.hersan+github@gmail.com>
2022-06-15 17:55:34 -0400

    adds language url hashes

--- src/Main.svelte
+++ src/Main.svelte
@@ -2,12 +2,20 @@
   import { readable } from "svelte/store";
   import PrototypeApp from "./prototype-app/PrototypeApp.svelte";
   import App from "./App.svelte";
+  import { language } from "./stores.js";
+  import { Languages } from "./types/cordiais.types";
 
   const hash = readable(window.location.hash, (set) => {
     const update = () => set(window.location.hash);
     window.addEventListener("hashchange", update);
     return () => window.removeEventListener("hashchange", update);
   });
+
+  if ($hash.endsWith("en")) {
+    $language = Languages.EN;
+  } else if ($hash.endsWith("pt")) {
+    $language = Languages.PT;
+  }
 </script>
 
 {#if $hash.endsWith("#/pesquisa") || $hash.endsWith("#pesquisa")}
